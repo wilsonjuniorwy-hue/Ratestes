d75389d677caf5d21095c154efbb731b0d67175a
Wagner Torres <wilsonjuniorwy@gmail.com>
2026-09-20 09:27:40 -0300

    test(e2e): aumenta timeout do teste para 90s

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   /* Maximum time one test can run for. */
-  timeout: 45000,
+  timeout: 90000,
   expect: {
     timeout: 10000,
   },
